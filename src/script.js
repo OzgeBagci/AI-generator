2d714373bba69ec71453981443e5f28d42a506ae
OzgeBagci <egzobagci@gmail.com>
2024-01-26 10:49:21 +0100

    Updated context

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function generateTravel(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "1494f5f7ob4a4dtd10b403df6ba9a052";
   let context =
-    "You are a well traveled individual who loves to share your insites on different travel destinatins. Your mission is to write 2 paragraphs about traveling to specific locations. Use basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a well traveled individual who loves to share your insites on different travel destinatins. Your mission is to write 2 short paragraphs about traveling to specific locations. Use basic HTML and separate each line with a <br />. Seperate each paragrapgh with a <br />. Do not include a title. Sign the last paragrapgh with 'SheCodes AI' inside a <strong> element at the very bottom and NOT at the beginning and NOT after the text. Make sure to follow user instructions.";
   let prompt = `User instructions: Generate travel information based on ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("generating travel");
